cd85cbb4b2c44bd92615ada7e8da71ed55e8f67d
Fogo <melatto.fogo@live.com>
2023-06-11 12:44:05 -0300

    feat(main.py): create main and read .env

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 # main.py
+from dotenv import dotenv_values
+
+env_vars = dotenv_values(".env")
+
+telegram_bot_token = env_vars["TELEGRAM_BOT_TOKEN"]
+telegram_chat_id = env_vars["TELEGRAM_CHAT_ID"]
+
+def main():
+    print(telegram_bot_token)
+    print(telegram_chat_id)
+
+if __name__ == '__main__':
+    main()
