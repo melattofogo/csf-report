38995b5b531ec38df952e64ac99d615560cce9fd
Fogo <melatto.fogo@live.com>
2023-06-14 00:07:15 -0300

    feat(utils.py): create create_simple_blocks_from_content

--- utils.py
+++ utils.py
@@ -9,3 +9,29 @@ def write_dict_to_file_as_json(content, file_name):
 def read_text(client, page_id):
     response = client.blocks.children.list(block_id=page_id)
     return response['results']
+
+def create_simple_blocks_from_content(client, content):
+    page_simple_blocks = []
+    for block in content:
+        
+        block_id = block['id']
+        block_type = block['type']
+        has_children = block['has_children']
+        rich_text = block[block_type]['rich_text']
+
+        if not rich_text:
+            return
+        
+        simple_block = {
+            'id': block_id,
+            'type': block_type,
+            'text': rich_text[0]['plain_text']
+        }
+
+        if has_children:
+            nested_children = read_text(client, block_id)
+            simple_block['children'] = create_simple_blocks_from_content(client, nested_children)
+        
+        page_simple_blocks.append(simple_block)
+
+        return page_simple_blocks
