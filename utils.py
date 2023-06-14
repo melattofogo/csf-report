51db306c86de2fe7e0c245caf16fe07d7030c5cd
Fogo <melatto.fogo@live.com>
2023-06-14 00:15:06 -0300

    feat(utils.py): create safe_get

--- utils.py
+++ utils.py
@@ -35,3 +35,19 @@ def create_simple_blocks_from_content(client, content):
         page_simple_blocks.append(simple_block)
 
         return page_simple_blocks
+
+def safe_get(data, dot_chained_keys):
+    '''
+        {'a': {'b': [{'c': 1}]}}
+        safe_get(data, 'a.b.0.c') -> 1
+    '''
+    keys = dot_chained_keys.split('.')
+    for key in keys:
+        try:
+            if isinstance(data, list):
+                data = data[int(key)]
+            else:
+                data = data[key]
+        except (KeyError, TypeError, IndexError):
+            return None
+    return data
