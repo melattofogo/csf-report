a7ccea51e508c9ecc116e6ebabc0cdbabf936d4d
Fogo <melatto.fogo@live.com>
2023-06-14 00:20:44 -0300

    feat(main.py): create notion client instance and test report_cesta

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import dotenv_values
 import requests
 from notion_client import Client
 import httpx
+from reports import *
 
 env_vars = dotenv_values(".env")
 
@@ -13,7 +14,18 @@ notion_page_id = env_vars["NOTION_PAGE_ID"]
 notion_db_id = env_vars["NOTION_DB_ID"]
 
 def main():
-    send_telegram_message('message')
+    try:
+        notion = Client(auth=notion_token)
+        users_list_response = notion.users.list()
+    except:
+        notion = Client(auth=notion_token, client=httpx.Client(verify=False))
+        users_list_response = notion.users.list()
+
+    result = report_cesta(notion, notion_db_id, 'Dia 5')
+    send_telegram_message(result)
+
+    result = report_cesta(notion, notion_db_id, 'Dia 20')
+    send_telegram_message(result)
 
 def send_telegram_message(message, token=telegram_bot_token, chat_id=telegram_chat_id):
     url = f"https://api.telegram.org/bot{token}/sendMessage"
