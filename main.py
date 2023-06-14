7dda572bb57964a4687f60f6c95f96388dc42ffa
Fogo <melatto.fogo@live.com>
2023-06-14 01:56:29 -0300

    doc(main.py): add comments

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ from notion_client import Client
 import httpx
 from reports import *
 
+# Load environment variables from .env file
 env_vars = dotenv_values(".env")
 
+# Get the required environment variables
 telegram_bot_token = env_vars["TELEGRAM_BOT_TOKEN"]
 telegram_chat_id = env_vars["TELEGRAM_CHAT_ID"]
 notion_token = env_vars["NOTION_API_TOKEN"]
@@ -15,9 +17,11 @@ notion_db_id = env_vars["NOTION_DB_ID"]
 
 def main():
     try:
+        # Initialize Notion client with API token
         notion = Client(auth=notion_token)
         users_list_response = notion.users.list()
     except:
+        # Initialize Notion client with API token and custom HTTP client (disable SSL verification)
         notion = Client(auth=notion_token, client=httpx.Client(verify=False))
         users_list_response = notion.users.list()
 
@@ -40,14 +44,21 @@ def main():
     send_telegram_message(result)
 
 def send_telegram_message(message, token=telegram_bot_token, chat_id=telegram_chat_id):
+    """
+    Send a message to a Telegram chat using the Telegram Bot API
+    """
+    # Telegram Bot API endpoint URL
     url = f"https://api.telegram.org/bot{token}/sendMessage"
+    # Data payload for the request
     data = {
         "chat_id": chat_id,
         "text": message
     }
     try:
+        # Send a POST request to the Telegram Bot API endpoint
         response = requests.post(url, json=data)
     except:
+        # Send a POST request to the Telegram Bot API endpoint with SSL verification disabled
         response = requests.post(url, json=data, verify=False)
 
     if response.status_code != 200:
